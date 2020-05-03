8ba1183729813e366aea14a529b31c0d0eb2f4e2
Anushka Narula <44411843+anushkanarula@users.noreply.github.com>
2020-05-03 11:50:42 +0530

    Update 02.cpp

--- 02.cpp
+++ 02.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) {
+        unordered_set<char>s;
+        int count=0;
+        for(int i=0;i<J.size();i++)
+        {
+            s.insert(J[i]);
+        }
+        for(int i=0;i<S.size();i++)
+        {
+           if(s.find(S[i])!=s.end())
+                count++;
+        }
+        return count;
+        
+    }
+};
