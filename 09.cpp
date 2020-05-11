09d9c830efbb224ddbc1718531336d563e94e79c
Anushka Narula <44411843+anushkanarula@users.noreply.github.com>
2020-05-12 00:12:34 +0530

    Update 09.cpp

--- 09.cpp
+++ 09.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int a=1;
+        while(num>0)
+        {
+         num=num-a; 
+         a=a+2;
+         }
+      return num==0;
+    }
+};
